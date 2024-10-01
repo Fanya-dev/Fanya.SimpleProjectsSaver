3357a648a64e4866b7013afa1d0191ba27f7e58e
Fanya <andrey2556@fanyplay.ru>
2024-10-01 22:26:19 +0300

    init

---
+++ Modules/Backup.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fanya.SimpleProjectsSaver.Modules
+{
+    public static class Backup
+    {
+        public static Dictionary<string, List<string>> Derictories = new();
+        public static Dictionary<string, List<string>> Files = new();
+        public static int count = new();
+        public static void Start()
+        {
+            DateTime now = DateTime.Now;
+            string currentPath = Directory.GetCurrentDirectory();
+            string backupPath = currentPath + "/backup_" + now.Day + "_" + now.Month + "_" + now.Year + "_" + now.Hour + "_" + now.Minute;
+            count = currentPath.Length;
+            Console.WriteLine("Собираю данные...");
+            GetFoldersAndFiles(currentPath, 1);
+            Directory.CreateDirectory(backupPath);
+            Console.WriteLine("Создание бэкапа...");
+            foreach (string folder in Program.CopyFolder)
+            {
+                Directory.CreateDirectory(backupPath + folder);
+            }
+            foreach (string file in Program.CopyFiles)
+            {
+                File.Copy(currentPath + file, backupPath + file, false);
+            }
+            
+        }
+
+        public static void GetFoldersAndFiles(string path, int level)
+        {
+            Derictories[path] = Directory.GetDirectories(path).ToList();
+            Files[path] = Directory.GetFiles(path).ToList();
+
+            foreach (string directory in Derictories[path])
+            {
+                string localPathDirectory = directory.Remove(0, count);
+                //for (int i = 0; i < level; i++) DEBUG
+                //{
+                //    Console.Write(".");
+                //}
+                //Console.WriteLine(directory);
+                Program.CopyFolder.Add(localPathDirectory);
+                GetFoldersAndFiles(directory, (level + 1));
+                if((directory.Contains("\\.git") || directory.Contains("/.git")) && !(directory.Contains("\\.git\\") || directory.Contains("/.git/"))) break;
+            }
+
+            foreach (string file in Files[path])
+            {
+                string localPathFile = file.Remove(0, count);
+                Program.CopyFiles.Add(localPathFile);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using Fanya.SimpleProjectsSaver.Modules;
+using System;
+using System.Text;
+
+namespace Fanya.SimpleProjectsSaver;
+
+public class Program
+{
+    public static List<string> CopyFolder = new();
+    public static List<string> CopyFiles = new();
+    public static void Main()
+    {
+        Console.InputEncoding = UTF8Encoding.UTF8;
+        Console.OutputEncoding = UTF8Encoding.UTF8;
+        Console.WriteLine("Создание бэкапа проектов");
+        Console.WriteLine("Если программа увидит в папке папку .git сохранит только эту папку и папку .git");
+        Console.WriteLine("Иначе полностью скопирует папку и данные в ней.");
+        Console.WriteLine("Меню:");
+        Console.WriteLine("0. Закрыть программу");
+        Console.WriteLine("1. Сделать бэкап из текущей папки");
+        Console.Write("Выберите пункт в меню: ");
+        string? MenuNumber = Console.ReadLine();
+
+        switch(MenuNumber)
+        {
+            case "1":
+                Backup.Start();
+                Console.WriteLine("Готово, нажмите любую клавишу чтобы закрыть программу.");
+                Console.Read();
+                break;
+                default:
+                break;
+        }
+    }
+}
